7511ac1d017974d0557cdc4031daa23521133ee0
yashhguptaa1 <yashhguptaa1@gmail.com>
2020-06-28 13:51:33 +0530

    Revised Rotate Array Question

--- src/Unit2_Arrays/Easy/LC189_RotateArray/NaiveSolution.java
+++ src/Unit2_Arrays/Easy/LC189_RotateArray/NaiveSolution.java
@@ -9,25 +9,11 @@ import java.util.Arrays;
 
 import static junit.framework.TestCase.failNotEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class NaiveSolution {
 
-    public void checkArrayEquality(String message,int expected[],int actual[]) {
-        if(Arrays.equals(expected,actual))
-            return;
 
-        failNotEquals(message, expected, actual);
-    }
-
-    /*public void assertEquals(String message, Object expected, Object actual) {
-        if (expected == null && actual == null) {
-            return;
-        }
-        if (expected != null && expected.equals(actual) &&Arrays.equals(expected,actual) ) {
-            return;
-        }
-        failNotEquals(message, expected, actual);
-    }*/
     public int[] rotate(int[] nums, int k) {
 
         int n=nums.length;
@@ -35,7 +21,7 @@ public class NaiveSolution {
         if(n==0 || k==0)
             return nums;
 
-        //k=k%n;
+        k=k%n;
 
         while(k-- > 0)
         {
@@ -58,16 +44,15 @@ public class NaiveSolution {
         int ip[]=new int[]{3};
         final int actual[]=rotate(ip,1);
         final int expected[] = new int[]{3};
-        //final int expected[] = new int[]{4};// prints wrong answer
-        checkArrayEquality("Wrong Answer",expected, actual);
+        assertTrue(Arrays.equals(expected,actual));
     }
     @Test
     public void sampleTestCase() {
         int ip[]=new int[]{1,2,3,4,5,6,7};
         final int actual[]=rotate(ip,3);
         final int expected[] = new int[]{5,6,7,1,2,3,4};
-        //final int expected[] = new int[]{4};// prints wrong answer
-        checkArrayEquality("Wrong Answer",expected, actual);
+        assertTrue(Arrays.equals(expected,actual));
+
 
     } @Test
     public void kBiggerThanSizeOfArray() {
@@ -75,23 +60,23 @@ public class NaiveSolution {
         final int actual[]=rotate(ip,7);
         System.out.println(Arrays.toString(actual));
         final int expected[] = new int[]{4,5,1,2,3};
-        //final int expected[] = new int[]{4};// prints wrong answer
-        checkArrayEquality("Wrong Answer",expected, actual);
+        assertTrue(Arrays.equals(expected,actual));
+
     } @Test
     public void EmptyTestCase() {
         int ip[]=new int[]{};
         final int actual[]=rotate(ip,1);
         final int expected[] = new int[]{};
-        //final int expected[] = new int[]{4};// prints wrong answer
-        checkArrayEquality("Wrong Answer",expected, actual);
+        assertTrue(Arrays.equals(expected,actual));
+
     }
     @Test
     public void kSameAsSize() {
         int ip[]=new int[]{1,2,3,4,5,6,7};
         final int actual[]=rotate(ip,7);
         final int expected[] = new int[]{1,2,3,4,5,6,7};
-        //final int expected[] = new int[]{4};// prints wrong answer
-        checkArrayEquality("Wrong Answer",expected, actual);
+        assertTrue(Arrays.equals(expected,actual));
+
     }
 
 
--- src/Unit2_Arrays/Easy/LC189_RotateArray/OptimizedSolution.java
+++ src/Unit2_Arrays/Easy/LC189_RotateArray/OptimizedSolution.java
@@ -8,26 +8,11 @@ import org.junit.runner.notification.Failure;
 import java.util.Arrays;
 
 import static junit.framework.TestCase.failNotEquals;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class OptimizedSolution {
 
-    public void checkArrayEquality(String message,int expected[],int actual[]) {
-        if(Arrays.equals(expected,actual))
-            return;
 
-        failNotEquals(message, expected, actual);
-    }
-
-    /*public void assertEquals(String message, Object expected, Object actual) {
-        if (expected == null && actual == null) {
-            return;
-        }
-        if (expected != null && expected.equals(actual) &&Arrays.equals(expected,actual) ) {
-            return;
-        }
-        failNotEquals(message, expected, actual);
-    }*/
     public int[] rotate(int[] nums, int k) {
 
         int n=nums.length;
@@ -67,16 +52,15 @@ public class OptimizedSolution {
         int ip[]=new int[]{3};
         final int actual[]=rotate(ip,1);
         final int expected[] = new int[]{3};
-        //final int expected[] = new int[]{4};// prints wrong answer
-        checkArrayEquality("Wrong Answer",expected, actual);
+        assertTrue(Arrays.equals(expected,actual));
+
     }
     @Test
     public void sampleTestCase() {
         int ip[]=new int[]{1,2,3,4,5,6,7};
         final int actual[]=rotate(ip,3);
         final int expected[] = new int[]{5,6,7,1,2,3,4};
-        //final int expected[] = new int[]{4};// prints wrong answer
-        checkArrayEquality("Wrong Answer",expected, actual);
+        assertTrue(Arrays.equals(expected,actual));
 
     } @Test
     public void kBiggerThanSizeOfArray() {
@@ -84,31 +68,31 @@ public class OptimizedSolution {
         final int actual[]=rotate(ip,7);
         System.out.println(Arrays.toString(actual));
         final int expected[] = new int[]{4,5,1,2,3};
-        //final int expected[] = new int[]{4};// prints wrong answer
-        checkArrayEquality("Wrong Answer",expected, actual);
+        assertTrue(Arrays.equals(expected,actual));
+
     } @Test
     public void EmptyTestCase() {
         int ip[]=new int[]{};
         final int actual[]=rotate(ip,1);
         final int expected[] = new int[]{};
-        //final int expected[] = new int[]{4};// prints wrong answer
-        checkArrayEquality("Wrong Answer",expected, actual);
+        assertTrue(Arrays.equals(expected,actual));
+
     }
     @Test
     public void kSameAsSize() {
         int ip[]=new int[]{1,2,3,4,5,6,7};
         final int actual[]=rotate(ip,7);
         final int expected[] = new int[]{1,2,3,4,5,6,7};
-        //final int expected[] = new int[]{4};// prints wrong answer
-        checkArrayEquality("Wrong Answer",expected, actual);
+        assertTrue(Arrays.equals(expected,actual));
+
     }
     @Test
     public void kZero() {
         int ip[]=new int[]{1,2,3,4,5,6,7};
         final int actual[]=rotate(ip,0);
         final int expected[] = new int[]{1,2,3,4,5,6,7};
-        //final int expected[] = new int[]{4};// prints wrong answer
-        checkArrayEquality("Wrong Answer",expected, actual);
+        assertTrue(Arrays.equals(expected,actual));
+
     }
 
 
